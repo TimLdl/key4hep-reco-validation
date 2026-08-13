89b3cece5200ebd38a54f513caae94cf17f7ba6c
Tim Leidel <tim.leidel@cern.ch>
2026-08-13 13:00:30 +0200

    Fixed wrong directory level reference

--- scripts/k4_reco_val_pipeline_utils/send_mail.py
+++ scripts/k4_reco_val_pipeline_utils/send_mail.py
@@ -5,7 +5,7 @@ from email.message import EmailMessage
 from pathlib import Path
 import smtplib
 
-SCRIPTS_DIR = Path(__file__).resolve().parents[2]
+SCRIPTS_DIR = Path(__file__).resolve().parents[1]
 if str(SCRIPTS_DIR) not in sys.path:
     sys.path.insert(0, str(SCRIPTS_DIR))
 
